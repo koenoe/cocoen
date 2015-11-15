783404a30daecba134bb54a542bff5718f386ca6
Koen Romers <info@koenromers.com>
2015-11-15 13:23:52 +0100

    feat(requestAnimationFrame): add polyfill to support IE9

--- src/js/cocoen.js
+++ src/js/cocoen.js
@@ -1,15 +1,5 @@
 ;(function ( $, window, document, undefined ) {
 
-	// shim layer with setTimeout fallback
-	window.requestAnimFrame = (function(){
-	  return  window.requestAnimationFrame       ||
-			  window.webkitRequestAnimationFrame ||
-			  window.mozRequestAnimationFrame    ||
-			  function( callback ){
-				window.setTimeout(callback, 1000 / 60);
-			  };
-	})();
-
 	'use strict';
 
 	var pluginName = 'cocoen',
