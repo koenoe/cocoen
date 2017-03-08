b620b75469882da508f78e6cde2a0f8294eaeb66
Koen Romers <info@koenromers.com>
2017-03-08 23:11:37 +0100

    fix(eslint): add gulp folder

--- gulp/tasks/eslint.js
+++ gulp/tasks/eslint.js
@@ -3,7 +3,7 @@ import gulp from 'gulp';
 import eslint from 'gulp-eslint';
 import config from '../config';
 
-gulp.task('eslint', () => gulp.src(config.files.js)
+gulp.task('eslint', () => gulp.src([].concat(config.files.js, ['./gulp/**/*.js']))
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError()));
