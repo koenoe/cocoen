30a1f189d023c821b71e79a4b818f3852930bebd
Koen Romers <info@koenromers.com>
2017-03-10 22:30:08 +0100

    feat(js): init methods

--- src/js/cocoen.js
+++ src/js/cocoen.js
@@ -1,11 +1,54 @@
+const init = Symbol('init');
+const createElements = Symbol('createElements');
+const setDimensions = Symbol('setDimensions');
+const addEventListeners = Symbol('addEventListeners');
+
 class Cocoen {
-  constructor() {
-    this.init();
+  constructor(element, options) {
+    this.options = Object.assign({}, Cocoen.defaults, options);
+    this.element = element || document.querySelector('.cocoen');
+
+    this[init]();
   }
 
-  init() {
-    console.log('Cocoen:init', this); // eslint-disable-line
+  [init]() {
+    this[createElements]();
+    this[setDimensions]();
+    this[addEventListeners]();
+  }
+
+  [createElements]() {
+    // Create drag element
+    const span = document.createElement('span');
+    span.className = this.options.dragElementSelector.replace('.', '');
+    this.element.appendChild(span);
+    // Wrap first image in div
+    const wrapper = document.createElement('div');
+    const firstImage = this.element.querySelector('img:first-child');
+    firstImage.before(wrapper);
+    wrapper.append(firstImage);
+    // Set class elements we need later
+    this.dragElement = this.element.querySelector(this.options.dragElementSelector);
+    this.beforeElement = this.element.querySelector('div:first-child');
+    this.beforeImage = this.beforeElement.querySelector('img');
+  }
+
+  [setDimensions]() {
+    this.elementWidth = parseInt(window.getComputedStyle(this.element).width, 10);
+    this.elementOffsetLeft = this.element.offsetLeft;
+    this.beforeImage.style.width = this.elementWidth;
+    this.dragElementWidth = parseInt(window.getComputedStyle(this.dragElement).width, 10);
+    this.minLeftPos = this.elementOffsetLeft + 10;
+    this.maxLeftPos = (this.elementOffsetLeft + this.elementWidth) - this.dragElementWidth - 10;
+  }
+
+  [addEventListeners]() {
+    console.log('addEventListeners', this); // eslint-disable-line
   }
 }
+
+Cocoen.defaults = {
+  dragElementSelector: '.cocoen__drag',
+};
 
 module.exports = Cocoen;
