37d0efad28d62874c0d9048003a453ab6d61efee
Koen Romers <info@koenromers.com>
2021-03-26 11:18:50 +0100

    v3 is coming!

--- gulp/tasks/clean.js
+++
@@ -1,6 +0,0 @@
-/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
-import gulp from 'gulp';
-import del from 'del';
-import config from '../config';
-
-gulp.task('clean', () => del([config.directories.build, `${config.directories.demo}/js`, `${config.directories.demo}/css`]));
