6db8d1ac001fa4f59bc5e3758464fbc2c1a28faa
Koen Romers <info@koenromers.com>
2021-03-28 18:31:26 +0200

    state of Cocoens

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,12 @@ const componentName = `${name}-component`;
 // Define our custom Cocoen web component
 customElements.define(componentName, Cocoen);
 
-// const state = {
-//   cocoens: [],
-// };
+type State = {
+  cocoens: Array<Cocoen>;
+};
+const state: State = {
+  cocoens: [],
+};
 
 const onDOMContentLoaded = () => {
   document.removeEventListener('DOMContentLoaded', onDOMContentLoaded);
@@ -16,7 +19,7 @@ const onDOMContentLoaded = () => {
 document.addEventListener('DOMContentLoaded', onDOMContentLoaded);
 
 export const create = (element: Element): void => {
-  const component = document.createElement(componentName);
+  const component = document.createElement(componentName) as Cocoen;
   const before = element.querySelectorAll('img')[0];
   const after = element.querySelectorAll('img')[1];
   before.setAttribute('slot', 'before');
@@ -24,6 +27,8 @@ export const create = (element: Element): void => {
   component.append(before.cloneNode(true));
   component.append(after.cloneNode(true));
   element.replaceWith(component);
+
+  state.cocoens.push(component);
 };
 
 export const parse = (context: HTMLElement): void => {
