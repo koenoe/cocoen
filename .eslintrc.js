a41546391ca5978887dbfbc9de8130f6ffcfd453
Koen Romers <info@koenromers.com>
2021-03-29 23:42:33 +0200

    use exact versions

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     'unicorn',
   ],
   extends: [
-    'airbnb-typescript',
+    'airbnb-typescript/base',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:eslint-comments/recommended',
