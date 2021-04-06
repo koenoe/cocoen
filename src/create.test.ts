1ad6226ef04cf7af3c8f71d3a5c7f1b898c6d403
Koen Romers <info@koenromers.com>
2021-04-06 15:51:25 +0200

    feature: support options object to  too

--- src/create.test.ts
+++ src/create.test.ts
@@ -19,6 +19,47 @@ describe('create', () => {
     expect(images).toHaveLength(2);
   });
 
+  test('should pass data attributes', () => {
+    const div = document.createElement('div');
+
+    div.innerHTML = `
+      <img src="before.jpg" alt="" />
+      <img src="after.jpg" alt="" />
+    `;
+    div.dataset.start = String(75);
+
+    const component = create(div);
+
+    expect(component.getAttribute('start')).toEqual(div.dataset.start);
+  });
+
+  test('should pass options', () => {
+    const div = document.createElement('div');
+
+    div.innerHTML = `
+      <img src="before.jpg" alt="" />
+      <img src="after.jpg" alt="" />
+    `;
+
+    const component = create(div, { color: 'red' });
+
+    expect(component.getAttribute('color')).toEqual('red');
+  });
+
+  test('should prioritise options over data attributes', () => {
+    const div = document.createElement('div');
+
+    div.innerHTML = `
+      <img src="before.jpg" alt="" />
+      <img src="after.jpg" alt="" />
+    `;
+    div.dataset.start = String(75);
+
+    const component = create(div, { start: 25 });
+
+    expect(component.getAttribute('start')).toEqual('25');
+  });
+
   test("should throw error if element doesn't contain two images", () => {
     const div = document.createElement('div');
 
